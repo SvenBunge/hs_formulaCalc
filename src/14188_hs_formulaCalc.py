15c4ed109aab4bb6cee5e35dc7595ba71580b8ec
Sven Bunge <sven@zeile7.de>
2022-07-18 09:50:29 +0200

    Add 2 more inputs, 2 more outputs / formula, fixed SBC, smaller improvements

--- src/14188_hs_formulaCalc.py
+++ src/14188_hs_formulaCalc.py
@@ -12,7 +12,7 @@ class Hs_formulaCalc14188(hsl20_3.BaseModule):
         hsl20_3.BaseModule.__init__(self, homeserver_context, "hs_formulaCalc14188")
         self.FRAMEWORK = self._get_framework()
         self.LOGGER = self._get_logger(hsl20_3.LOGGING_NONE,())
-        self.PIN_I_FORMULA=1
+        self.PIN_I_X0=1
         self.PIN_I_X1=2
         self.PIN_I_X2=3
         self.PIN_I_X3=4
@@ -21,16 +21,24 @@ class Hs_formulaCalc14188(hsl20_3.BaseModule):
         self.PIN_I_X6=7
         self.PIN_I_X7=8
         self.PIN_I_X8=9
-        self.PIN_O_FORMULA_OUTPUT=1
-        self.PIN_O_ERROR=2
-        self.PIN_O_DEBUG=3
+        self.PIN_I_X9=10
+        self.PIN_I_FORMULA_0=11
+        self.PIN_I_FORMULA_1=12
+        self.PIN_I_FORMULA_2=13
+        self.PIN_O_FORMULA_OUTPUT_Y0=1
+        self.PIN_O_FORMULA_OUTPUT_Y1=2
+        self.PIN_O_FORMULA_OUTPUT_Y2=3
+        self.PIN_O_ERROR=4
+        self.PIN_O_DEBUG=5
         self.FRAMEWORK._run_in_context_thread(self.on_init)
 
 ########################################################################################################
 #### Own written code can be placed after this commentblock . Do not change or delete commentblock! ####
 ###################################################################################################!!!##
 
-        self.last_value = -1000
+        self.last_value_y0 = float('nan')
+        self.last_value_y1 = float('nan')
+        self.last_value_y2 = float('nan')
         self.method_dict = {}
 
     def on_init(self):
@@ -59,14 +67,33 @@ class Hs_formulaCalc14188(hsl20_3.BaseModule):
         self.calculate_formula()  # run calc on every update
 
     def calculate_formula(self):
-        formula, x1, x2, x3, x4, x5, x6, x7, x8 = self.get_inputs()
-        formula = self.add_security(formula)
+        x0, x1, x2, x3, x4, x5, x6, x7, x8, x9, formula_0, formula_1, formula_2 = self.get_inputs()
+        formula_0 = self.add_security(formula_0)
+        formula_1 = self.add_security(formula_1)
+        formula_2 = self.add_security(formula_2)
         try:
-            value_dict = {"x1": x1, "x2": x2, "x3": x3, "x4": x4, "x5": x5, "x6": x6, "x7": x7, "x8": x8}
+            value_dict = {"x0": x0, "x1": x1, "x2": x2, "x3": x3, "x4": x4, "x5": x5, "x6": x6, "x7": x7, "x8": x8, "x9": x9}
+            value_dict.update(self.create_result_dict())
             value_dict.update(self.method_dict)  # Merge methods and vars into eval env
-            result = eval(formula, value_dict)
-            if result != self.last_value:  # sbc
-                self._set_output_value(self.PIN_O_FORMULA_OUTPUT, result)
+            if formula_0:
+                result = eval(formula_0, value_dict)
+                if result != self.last_value_y0:  # sbc
+                    self._set_output_value(self.PIN_O_FORMULA_OUTPUT_Y0, result)
+                    self.last_value_y0 = result
+
+            value_dict.update(self.create_result_dict())  # Update y values from calculations
+            if formula_1:
+                result = eval(formula_1, value_dict)
+                if result != self.last_value_y1:  # sbc
+                    self._set_output_value(self.PIN_O_FORMULA_OUTPUT_Y1, result)
+                    self.last_value_y1 = result
+
+            value_dict.update(self.create_result_dict())  # Update y values from calculations
+            if formula_2:
+                result = eval(formula_2, value_dict)
+                if result != self.last_value_y1:  # sbc
+                    self._set_output_value(self.PIN_O_FORMULA_OUTPUT_Y2, result)
+                    self.last_value_y2 = result
 
             self._set_output_value(self.PIN_O_ERROR, 0)
             self._set_output_value(self.PIN_O_DEBUG, "")
@@ -75,7 +102,7 @@ class Hs_formulaCalc14188(hsl20_3.BaseModule):
             self._set_output_value(self.PIN_O_ERROR, 1)
 
     def get_inputs(self):
-        formula = self._get_input_value(self.PIN_I_FORMULA)
+        x0 = int(self._get_input_value(self.PIN_I_X0))
         x1 = int(self._get_input_value(self.PIN_I_X1))
         x2 = int(self._get_input_value(self.PIN_I_X2))
         x3 = int(self._get_input_value(self.PIN_I_X3))
@@ -84,7 +111,11 @@ class Hs_formulaCalc14188(hsl20_3.BaseModule):
         x6 = int(self._get_input_value(self.PIN_I_X6))
         x7 = int(self._get_input_value(self.PIN_I_X7))
         x8 = int(self._get_input_value(self.PIN_I_X8))
-        return formula, x1, x2, x3, x4, x5, x6, x7, x8
+        x9 = int(self._get_input_value(self.PIN_I_X9))
+        formula_0 = self._get_input_value(self.PIN_I_FORMULA_0)
+        formula_1 = self._get_input_value(self.PIN_I_FORMULA_1)
+        formula_2 = self._get_input_value(self.PIN_I_FORMULA_2)
+        return x0, x1, x2, x3, x4, x5, x6, x7, x8, x9, formula_0, formula_1, formula_2
 
     def add_security(self, formula):
         # does not fix everything but makes me feel better
@@ -103,3 +134,14 @@ class Hs_formulaCalc14188(hsl20_3.BaseModule):
         formula = formula.replace("lambda", "")
         formula = formula.replace("repr", "")
         return formula
+
+    def create_result_dict(self):
+        result_dict = {}
+        if not math.isnan(self.last_value_y0):
+            result_dict['y0'] = self.last_value_y0
+        if not math.isnan(self.last_value_y1):
+            result_dict['y1'] = self.last_value_y1
+        if not math.isnan(self.last_value_y2):
+            result_dict['y2'] = self.last_value_y2
+
+        return result_dict
